e201497f37994c86ae6c77e627744e153807db87
chuvacagapj@hotmail.com <Jesus Garcia>
2015-05-04 21:43:06 -0600

    se hacen mas pruebas, y se agrega una imagen representativa del automata

--- AnalizadorLexico/src/test/java/jflex/test/LexerTest.java
+++ AnalizadorLexico/src/test/java/jflex/test/LexerTest.java
@@ -53,97 +53,145 @@ public class LexerTest {
     }
     
     @Test(expected = Error.class)
-    public void pruebaEnteroFalla(){
-        //TODO
+    public void pruebaEnteroFalla()throws IOException{
+        String ruta = "src/test/java/pruebas/pruebaEnteroFalla.chuy";
+        FileReader archivo = new FileReader(ruta);
+        List<Token> tokens = null;
+        
+        Lexer lex = new Lexer(archivo);
+        while(!lex.getzzAtEOF()){
+                lex.yylex();
+        }
+        
+        tokens = lex.analizar();
     }
     
     @Test
-    public void pruebaCadena(){
+    public void pruebaCadena()throws IOException{
+         String ruta = "src/test/java/pruebas/pruebaCadena.chuy";
+        FileReader archivo = new FileReader(ruta);
+        List<Token> tokens = null;
+        Token[] esperado = 
+            {   new Token(1, Token.ENTERO,     "0"       ),
+                new Token(1, Token.CADENA,     "\"hola\""),
+                new Token(1, Token.PUNTO_COMA, ";"       ),
+                new Token(2, Token.CADENA,     "\"20\""  ),
+                new Token(2, Token.PUNTO_COMA, ";"       ),
+                new Token(3, Token.CADENA,     "\"-\""   ),
+                new Token(3, Token.CADENA,     "\"0\""       ),
+                new Token(3, Token.PUNTO_COMA, ";"       ),
+                new Token(4, Token.ENTERO,     "-14"     ),
+                new Token(4, Token.PUNTO_COMA, ";"       ),
+                new Token(5, Token.ENTERO,     "+0"      ),
+                new Token(5, Token.PUNTO_COMA, ";"       ),
+                new Token(6, Token.ENTERO,     "+17"     ),
+                new Token(6, Token.PUNTO_COMA, ";"       ),
+                new Token(7, Token.CADENA,     "\"¡Bienvenidos al Estado Grande!\"")};
+        
+        Lexer lex = new Lexer(archivo);
+        while(!lex.getzzAtEOF()){
+                lex.yylex();
+        }
+        
+        tokens = lex.analizar();
+        
+        for (Token esperado1 :tokens) {
+            System.out.println(esperado1);
+        }
+        assertArrayEquals(esperado, tokens.toArray(new Token[tokens.toArray().length]));
+    }
+    
+    @Test(expected = Error.class)
+    public void pruebaCadenaFalla()throws IOException{
+        String ruta = "src/test/java/pruebas/pruebaCadenaFalla.chuy";
+        FileReader archivo = new FileReader(ruta);
+        List<Token> tokens = null;
+        
+        Lexer lex = new Lexer(archivo);
+        while(!lex.getzzAtEOF()){
+                lex.yylex();
+        }
+        
+        tokens = lex.analizar();
+    }
+    
+    @Test
+    public void pruebaAtomo1()throws IOException{
         //TODO
     }
     
     @Test//(expected = Error.class)
-    public void pruebaCadenaFala(){
+    public void pruebaAtomo1Falla()throws IOException{
         //TODO
     }
     
     @Test
-    public void pruebaAtomo1(){
+    public void pruebaAtomo2()throws IOException{
         //TODO
     }
     
     @Test//(expected = Error.class)
-    public void pruebaAtomo1Falla(){
+    public void pruebaAtomo2Falla()throws IOException{
         //TODO
     }
     
     @Test
-    public void pruebaAtomo2(){
+    public void pruebaAtomo3()throws IOException{
         //TODO
     }
     
     @Test//(expected = Error.class)
-    public void pruebaAtomo2Falla(){
+    public void pruebaAtomo3Falla()throws IOException{
         //TODO
     }
     
     @Test
-    public void pruebaAtomo3(){
+    public void pruebaPtoFijo()throws IOException{
         //TODO
     }
     
     @Test//(expected = Error.class)
-    public void pruebaAtomo3Falla(){
+    public void pruebaPtoFijoFalla()throws IOException{
         //TODO
     }
     
     @Test
-    public void pruebaPtoFijo(){
+    public void pruebaPtoFlot()throws IOException{
         //TODO
     }
     
     @Test//(expected = Error.class)
-    public void pruebaPtoFijoFalla(){
+    public void pruebaPtoFlotFalla()throws IOException{
         //TODO
     }
     
     @Test
-    public void pruebaPtoFlot(){
+    public void pruebaVariable()throws IOException{
         //TODO
     }
     
     @Test//(expected = Error.class)
-    public void pruebaPtoFlotFalla(){
+    public void pruebaVariableFalla()throws IOException{
         //TODO
     }
     
     @Test
-    public void pruebaVariable(){
+    public void pruebaPredicado()throws IOException{
         //TODO
     }
     
     @Test//(expected = Error.class)
-    public void pruebaVariableFalla(){
+    public void pruebaPredicadoFalla()throws IOException{
         //TODO
     }
     
     @Test
-    public void pruebaPredicado(){
+    public void pruebaLista()throws IOException{
         //TODO
     }
     
     @Test//(expected = Error.class)
-    public void pruebaPredicadoFalla(){
-        //TODO
-    }
-    
-    @Test
-    public void pruebaLista(){
-        //TODO
-    }
-    
-    @Test//(expected = Error.class)
-    public void pruebaListaFalla(){
+    public void pruebaListaFalla()throws IOException{
         //TODO
     }
 }
